aaa9dd4181a5ada384ab0d30f8e5f18d16e1035f
kzlsk <rlaflsk4887@gmail.com>
2025-05-02 17:40:26 +0900

    feat: authContext, useAuth 훅 구현

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,18 @@
+// auth 커스텀 훅
+
+import { useContext } from "react";
+import { AuthContext, AuthContextType } from "../store/AuthContext";
+
+// 훅 선언
+export default function useAuth(): AuthContextType {
+    // AuthContext를 useContext로 꺼내기
+    const context = useContext(AuthContext);
+
+    if(!context) {
+        console.log('실패');
+    }
+
+    // 임시 데이터 받아오는 것이기 때문에 타입 단언으로 undefined가
+    // 아니라는 것을 강제로 명시
+    return context as AuthContextType;
+};
