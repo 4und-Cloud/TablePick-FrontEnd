8484986ac4219ba49ced957f0bdd7a3a0d12f3fe
kzlsk <rlaflsk4887@gmail.com>
2025-05-02 17:40:26 +0900

    feat: authContext, useAuth 훅 구현

--- src/store/AuthContext.tsx
+++ src/store/AuthContext.tsx
@@ -1,12 +1,46 @@
-import React, {createContext, useContext} from "react";
+// 로그인 상태 관리
 
-interface AuthContextType {
-    isAuthenticated: boolean;
+import {createContext, ReactNode, useState} from "react";
+
+// 임시
+export interface AuthContextType {
+    isAuthenticated: boolean; // 로그인 여부
     user: {
         name: string;
         image? : string;
-    };
-    login: ( user: {name: string; image: string}) => void;
-    logout: () => void;
+    }; // 유저 정보
+    login: ( user: {name: string; image: string}) => void; // 로그인 함수 
+    logout: () => void; // 로그아웃 함수
 
 }
+
+// Context 생성
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+export default function AuthProvider({children} : AuthProviderProps) {
+    // 로그인 여부 관리
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    // 유저 정보 관리
+    const [user, setUser] = useState<{name: string; image: string}>({name: '', image:''});
+    // login
+    const login = (userData: {name: string; image: string}) => {
+        setIsAuthenticated(true);
+        setUser(userData);
+    };
+    // logout
+    const logout = () => {
+        setIsAuthenticated(false);
+        setUser({name: '', image: ''})
+    };
+    return(
+        <AuthContext.Provider value={{isAuthenticated, user, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+ }
+
+    
