37a7499c71498e59609c96d3679749f8342c6bf3
kzlsk <rlaflsk4887@gmail.com>
2025-05-07 00:14:45 +0900

    feat/공통 컴포넌트 버튼 구현 및 적용 / 헤더 조건부 렌더링 처리

--- src/components/Header/AuthHeader.tsx
+++ src/components/Header/AuthHeader.tsx
@@ -2,12 +2,13 @@ import useAuth  from "../../hooks/useAuth";
 import profile from '@/assets/images/profile_img.jpg';
 import logo from '@/assets/images/logo_text.png';
 import search from '@/assets/images/magnifying-glass.png';
+import RoundedBtn from "../Button/RoundedBtn";
 
 export default function AuthHeader() {
     const {logout} = useAuth();
 
     return(
-        <header className="bg-bg-main w-full h-[80px] flex items-center justify-between px-4 gap-4">
+        <header className="fixed top-0 left-0 z-50 bg-main w-full h-[80px] flex items-center justify-between px-4 gap-4">
                     {/* logo */}
                     <div className="flex items-center">
                         <img src={logo} className='h-[40px]'/>
@@ -26,7 +27,11 @@ export default function AuthHeader() {
                     <div className="flex items-center gap-4">
                         <img src={profile} 
                         className="w-[40px] h-[40px] rounded-full"/>
-                        <button onClick={logout} className='shadow-xl border-[2px] border-transparent h-[40px] px-[24px] py-[6px] rounded-[16px] bg-white hover:bg-bg-main hover:border-white hover:text-white'>Logout</button>
+                        <RoundedBtn 
+                            text="Logout" 
+                            width="w-[100px]"
+                            onClick={logout}
+                             />
                     </div>
                     
                 </header>
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import  AuthProvider  from './store/AuthContext.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
 <AuthProvider>
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 </AuthProvider>
 )
