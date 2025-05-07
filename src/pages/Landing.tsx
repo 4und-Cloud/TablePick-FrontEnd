4946557d063af7c78652f831d383be22a25f0555
kzlsk <rlaflsk4887@gmail.com>
2025-05-07 15:05:38 +0900

    feat/태그

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,7 +4,6 @@ export default function Landing(){
     return(
         <div className="pt-[90px]">
             <Tag text="주차하기 편해요"/>
-            <Tag text='조용하고 고급스럽고 암튼 짱 좋음' />
             <p>랜딩</p>
         </div>
         
