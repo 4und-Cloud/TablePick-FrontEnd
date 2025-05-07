ebb89888a3bd98d7244ceb96ba89e51acc5ece7d
kzlsk <rlaflsk4887@gmail.com>
2025-05-07 15:04:44 +0900

    feat/태그

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,24 +1,10 @@
-import RoundedBtn from "../components/Button/RoundedBtn";
-import Modal from "../components/Modal"
-import useModal from "../hooks/useModal";
+import Tag from "../components/Tag";
 
 export default function Landing(){
-    const {isOpen, closeModal, openModal} = useModal({initialState : false});
-
-    const handleClose = () =>{
-        closeModal();
-    }
     return(
         <div className="pt-[90px]">
-            <button onClick={openModal} className="border border-main">모달 열기 </button>
-            {isOpen && (<Modal onClose={handleClose}
-                footer = {
-                    <RoundedBtn text='적용하기'
-                    width="w-full"
-                    borderColor="border-main" />
-                }>
-                <p className="ml-2 text-[24px]">필터</p>
-            </Modal>)}
+            <Tag text="주차하기 편해요"/>
+            <Tag text='조용하고 고급스럽고 암튼 짱 좋음' />
             <p>랜딩</p>
         </div>
         
