bda9ff19eb2b388389d814f49c7e5e3596abf732
kzlsk <rlaflsk4887@gmail.com>
2025-05-07 09:57:09 +0900

    feat/필터 버튼 컴포넌트

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,12 @@
+import FilterBtn from "../components/Button/FilterBtn"
 
 
 export default function Landing(){
     return(
-        <>
+        <div className="pt-[90px]">
+            <FilterBtn />
             <p>랜딩</p>
-        </>
+        </div>
         
     )
 }
