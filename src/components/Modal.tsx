8b725cb2b1f0ffa11f0e329e0b1a4726678dca26
kzlsk <rlaflsk4887@gmail.com>
2025-05-08 16:03:47 +0900

    design/모달 수정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 interface ModalProps{
     onClose: () => void;
     children: React.ReactNode;
+    close? : React.ReactNode; // 닫기 버튼 자리
     footer? : React.ReactNode; // 하단 버튼 자리
 }
 
-export default function Modal( {onClose, children, footer } : ModalProps) {    
+export default function Modal( {onClose, children, footer, close } : ModalProps) {    
     return ReactDOM.createPortal(
         <div className='fixed inset-0 bg-black bg-opacity-30 flex items-center justify-center z-50'>
             <div className='bg-white p-2 rounded-20 shadow-xl relative w-[540px] h-[540px] border-2 border-main flex flex-col justify-between'>
-                <button className='absolute right-4 font-bold text-[20px] text-main' onClick={onClose}>X</button>
+                {close && (<button className='absolute right-4 font-bold text-[20px] text-main' onClick={onClose}>X</button>)}
                 {children}
                 {footer && (
                 <div className = 'p-4 flex jusify-end'>
