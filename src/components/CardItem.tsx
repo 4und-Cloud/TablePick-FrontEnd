f85f80101b9dfb48ade4e7cf86a0c2bc346bda8c
kzlsk <rlaflsk4887@gmail.com>
2025-05-09 13:23:55 +0900

    feat/dummy 데이터 넣어서 랜딩페이지 레이아웃 작업 완료

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,38 +1,51 @@
+import location from '@/assets/images/location.png'
+
 export interface CardItemProps{
     image? :string; // 이미지
-    restaurantName: string; // 식당명
+    restaurantName?: string; // 식당명
     description? : string; // 주소, 게시글 내용
     tags? : string[]; // 태그
     reservationInfo? : React.ReactNode; // 예약 관련 정보
     button? : React.ReactNode; // 버튼
     buttonPosition? : 'bottom'; // 버튼 위치
+    containerStyle? : string; // 카드 전체 wrapper 커스텀
+    imageStyle? : string; // 이미지 커스텀
+    restaurantNameStyle?: string; // 식당명 커스텀
 }
 
 export default function CardItem({
     image,
+    containerStyle,
+    imageStyle,
+    restaurantNameStyle,
     restaurantName,
     description,
     tags = [],
     reservationInfo,
     button,
     buttonPosition,
+
   }: CardItemProps) {
     return (
-      <div className="relative flex flex-col border w-[370px] rounded-xl shadow-xl p-3 bg-card mx-2 my-2 h-[335px] overflow-hidden">
+<div className={`relative flex flex-col p-3 mx-2 my-2 overflow-hidden ${containerStyle ?? 'border w-[370px] rounded-xl shadow-xl h-[335px] bg-card'}`}>
         {image && (
           <img
             src={image}
             alt={restaurantName}
-            className="w-full h-[200px] rounded-md object-cover mb-2"
+            className={`w-full h-[200px] rounded-md object-cover mb-2 ${imageStyle}`}
           />
         )}
   
         <div className="flex flex-col justify-between w-full overflow-hidden">
           <div className="flex flex-col overflow-hidden w-full">
             <div className="flex flex-col justify-between w-full">
-              <h2 className="text-lg font-bold w-full">
-                {restaurantName}
-              </h2>
+              <div className='flex flex-row items-center gap-2'>
+                <img src={location} className='w-[16px] h-[16px]' />
+                <span className={`text-lg font-bold w-full ${restaurantNameStyle}`}>
+                  {restaurantName}
+                </span>
+              </div>
+              
               {description && (
                 <span className="text-sm text-gray-600 w-full mt-[2px] truncate">
                   {description}
