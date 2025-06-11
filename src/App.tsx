61cdc1a3852c7571a0157267057b021fab8c7319
kzlsk <rlaflsk4887@gmail.com>
2025-06-11 15:47:42 +0900

    필요없는 파일 삭제 및 tanstack query 설치

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,11 @@ export default function App() {
   const { isAuthenticated } = useAuth();
   return (
     <div className="relative w-full min-h-screen flex justify-center bg-white">
-      {/* 전체 상단 보더 */}
-      {/* <div className="absolute top-0 left-0 w-full h-[97px] z-0 border-b border-main" /> */}
 
       {/* 실제 콘텐츠 영역 */}
       <div className="relative min-h-screen w-full bg-white">
         {isAuthenticated ? <AuthHeader /> : <UnAuthHeader />}
 
-        {/* 헤더 하단 보더 */}
-        {/* <div className="w-full h-px bg-main" /> */}
           <PageRouter />
         
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import App from './App'
 import  AuthProvider  from './store/AuthContext'
 import { BrowserRouter } from 'react-router-dom'
 import { TagProvider } from './store/TagContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-        <AuthProvider>
-            <TagProvider>
-                <App />
-            </TagProvider>
-        </AuthProvider>
+      <AuthProvider>
+        <TagProvider>
+          <App />
+        </TagProvider>
+      </AuthProvider>
     </BrowserRouter>
+  </QueryClientProvider>
 )
